cb8a66ceb88639fc232cbd2d44ee2bda89d1eae1
davidfrk <davida1d2@gmail.com>
2024-04-07 17:55:18 -0300

    Starting new project page

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,12 @@
+import ProjectForm from '../project/ProjectForm';
+import styles from './NewProject.module.css'
+
 function NewProject () {
   return (
-    <div id="new_project">
-      <p>Novo Projeto</p>
+    <div id="new_project" className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm />
     </div>
   )
 }
