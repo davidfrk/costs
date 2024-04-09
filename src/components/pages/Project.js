57df08a65ddbb321a67dae94cae7e0cd8f25b7aa
davidfrk <davida1d2@gmail.com>
2024-04-09 12:01:56 -0300

    Add services section

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -11,6 +11,7 @@ function Project() {
   const { id } = useParams()
   const [project, setProject] = useState({})
   const [showProjectForm, setShowProjectForm] = useState(false)
+  const [showServiceForm, setShowServiceForm] = useState(false)
   const [message, setMessage] = useState()
   const [messageType, setMessageType] = useState()
 
@@ -28,6 +29,8 @@ function Project() {
   }, [id])
 
   function editPost(project) {
+    setMessage('')
+    
     if(project.budget < project.cost) {
       setMessage('O orçamento não pode ser menor que o custo do projeto!')
       setMessageType('error')
@@ -52,6 +55,10 @@ function Project() {
 
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm)
+  }
+
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm)
   }
 
   return (
@@ -83,6 +90,23 @@ function Project() {
                 </div>
               )}
             </div>
+            <div className={styles.service_form_container}>
+              <h2>Adicione um serviço:</h2>
+              <button className={styles.button} onClick={toggleServiceForm}>
+                {!showServiceForm ? 'Adicionar serviço' : 'Fechar serviço'}
+              </button>
+              <div className={styles.project_info}>
+                {showServiceForm && (
+                  <div>
+                    Formulário de serviço
+                  </div>
+                )}
+              </div>
+            </div>
+            <h2>Serviços</h2>
+            <Container>
+              <p>Serviços</p>
+            </Container>
           </Container>
         </div>
       ) : <Loading/>}
